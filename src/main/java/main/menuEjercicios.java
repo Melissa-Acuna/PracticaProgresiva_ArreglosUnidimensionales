1cf25d13080ac38e31bfb877bf0f0d9cd945c3e2
Melissa1 <melissa.acunabonilla@ucr.ac.cr>
2026-09-10 21:39:25 -0600

    Estableci clase para menu y su esqueleto basico

--- src/main/java/main/menuEjercicios.java
+++ src/main/java/main/menuEjercicios.java
@@ -20,14 +20,38 @@ public class menuEjercicios {
     }
 
     public void menuGlobal() {
-        int optMenu = Integer.parseInt(JOptionPane.showInputDialog("***BIENVENIDO AL MENU DE EJECUCION DE LA PRACTICA PROGRESIVA DE ARREGLOS***"
+        int optMenu =0;
+        do {
+            optMenu = Integer.parseInt(JOptionPane.showInputDialog("***BIENVENIDO AL MENU DE EJECUCION DE LA PRACTICA PROGRESIVA DE ARREGLOS***"
                 + "\nSELECCIONE EL NIVEL QUE DESEA EJECUTAR:"
-                + "\n1. Nivel 1"
+                + "\n1. Nivel 1 -- Fundamentos"
                 + "\n....PROXIMAMENTE"
                 + "\n0.Salir"));
-        do {
             switch (optMenu) {
                 case 1:
+                    int optLVL1 =0;
+                    do {
+                        optLVL1 = Integer.parseInt(JOptionPane.showInputDialog("Seleccione el ejercicio que desea ejecutar: "
+                            + "\n1. Basico double"
+                            + "\n2. Basico boolean"
+                            + "\n3. Basico int"
+                            + "\n4. Basico char"
+                            + "\n5. Regresar al menu principal"));
+                        switch (optLVL1) {
+                            case 1:
+                                break;
+                            case 2:
+                                break;
+                            case 3:
+                                break;
+                            case 4:
+                                break;
+                            case 5:
+                                JOptionPane.showMessageDialog(null, "REGRESANDO AL MENÚ PRINCIPAL");
+                                break;
+                            default: JOptionPane.showMessageDialog(null, "No disponible.");
+                        }
+                    } while (optLVL1 != 5);
                     break;
                 case 2:
                     break;
@@ -40,7 +64,8 @@ public class menuEjercicios {
                 case 0:
                     JOptionPane.showMessageDialog(null, "Gracias por usar el sistema");
                     break;
-                default: JOptionPane.showMessageDialog(null, "No disponible.");
+                default:
+                    JOptionPane.showMessageDialog(null, "No disponible.");
             }
         } while (optMenu != 0);
     }
