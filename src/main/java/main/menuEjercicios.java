d81b29fc8a36d83eb32576db9a52c3c8100f2f4a
Melissa1 <melissa.acunabonilla@ucr.ac.cr>
2026-09-10 21:50:30 -0600

    Establecí clase para menú y su esqueleto funcional

--- src/main/java/main/menuEjercicios.java
+++ src/main/java/main/menuEjercicios.java
@@ -20,23 +20,26 @@ public class menuEjercicios {
     }
 
     public void menuGlobal() {
-        int optMenu =0;
+        int optMenu = 0;
         do {
             optMenu = Integer.parseInt(JOptionPane.showInputDialog("***BIENVENIDO AL MENU DE EJECUCION DE LA PRACTICA PROGRESIVA DE ARREGLOS***"
-                + "\nSELECCIONE EL NIVEL QUE DESEA EJECUTAR:"
-                + "\n1. Nivel 1 -- Fundamentos"
-                + "\n....PROXIMAMENTE"
-                + "\n0.Salir"));
+                    + "\nSELECCIONE EL NIVEL QUE DESEA EJECUTAR:"
+                    + "\n1. Nivel 1 -- Fundamentos"
+                    + "\n2. Nivel 2 -- Recorridos y Acumulación"
+                    + "\n3. Nivel 3 -- Busqueda, extremos y condiciones"
+                    + "\n4. Nivel 4 -- Transformación de arreglos"
+                    + "\n5. Nivel 5 -- Integración y retos"
+                    + "\n0.Salir"));
             switch (optMenu) {
                 case 1:
-                    int optLVL1 =0;
+                    int optLVL1 = 0;
                     do {
                         optLVL1 = Integer.parseInt(JOptionPane.showInputDialog("Seleccione el ejercicio que desea ejecutar: "
-                            + "\n1. Basico double"
-                            + "\n2. Basico boolean"
-                            + "\n3. Basico int"
-                            + "\n4. Basico char"
-                            + "\n5. Regresar al menu principal"));
+                                + "\n1. Basico double"
+                                + "\n2. Basico boolean"
+                                + "\n3. Basico int"
+                                + "\n4. Basico char"
+                                + "\n5. Regresar al menu principal"));
                         switch (optLVL1) {
                             case 1:
                                 break;
@@ -49,17 +52,114 @@ public class menuEjercicios {
                             case 5:
                                 JOptionPane.showMessageDialog(null, "REGRESANDO AL MENÚ PRINCIPAL");
                                 break;
-                            default: JOptionPane.showMessageDialog(null, "No disponible.");
+                            default:
+                                JOptionPane.showMessageDialog(null, "No disponible.");
                         }
                     } while (optLVL1 != 5);
                     break;
                 case 2:
+                    int optLVL2 = 0;
+                    do {
+                        optLVL2 = Integer.parseInt(JOptionPane.showInputDialog("Seleccione el ejercicio que desea ejecutar: "
+                                + "\n1. Basico-Intermedio double"
+                                + "\n2. Basico-Intermedio boolean"
+                                + "\n3. Basico-Intermedio char"
+                                + "\n4. Basico-Intermedio int"
+                                + "\n5. Regresar al menu principal"));
+                        switch (optLVL2) {
+                            case 1:
+                                break;
+                            case 2:
+                                break;
+                            case 3:
+                                break;
+                            case 4:
+                                break;
+                            case 5:
+                                JOptionPane.showMessageDialog(null, "REGRESANDO AL MENÚ PRINCIPAL");
+                                break;
+                            default:
+                                JOptionPane.showMessageDialog(null, "No disponible.");
+                        }
+                    } while (optLVL2 != 5);
                     break;
                 case 3:
+                    int optLVL3 = 0;
+                    do {
+                        optLVL3 = Integer.parseInt(JOptionPane.showInputDialog("Seleccione el ejercicio que desea ejecutar: "
+                                + "\n1. Intermedio int"
+                                + "\n2. Intermedio int"
+                                + "\n3. Intermedio int"
+                                + "\n4. Intermedio char"
+                                + "\n5. Regresar al menu principal"));
+                        switch (optLVL3) {
+                            case 1:
+                                break;
+                            case 2:
+                                break;
+                            case 3:
+                                break;
+                            case 4:
+                                break;
+                            case 5:
+                                JOptionPane.showMessageDialog(null, "REGRESANDO AL MENÚ PRINCIPAL");
+                                break;
+                            default:
+                                JOptionPane.showMessageDialog(null, "No disponible.");
+                        }
+                    } while (optLVL3 != 5);
                     break;
                 case 4:
+                    int optLVL4 = 0;
+                    do {
+                        optLVL4 = Integer.parseInt(JOptionPane.showInputDialog("Seleccione el ejercicio que desea ejecutar: "
+                                + "\n1. Intermedio-Alto int"
+                                + "\n2. Intermedio-Alto int"
+                                + "\n3. Intermedio-Alto int"
+                                + "\n4. Intermedio-Alto int"
+                                + "\n5. Regresar al menu principal"));
+                        switch (optLVL4) {
+                            case 1:
+                                break;
+                            case 2:
+                                break;
+                            case 3:
+                                break;
+                            case 4:
+                                break;
+                            case 5:
+                                JOptionPane.showMessageDialog(null, "REGRESANDO AL MENÚ PRINCIPAL");
+                                break;
+                            default:
+                                JOptionPane.showMessageDialog(null, "No disponible.");
+                        }
+                    } while (optLVL4 != 5);
                     break;
                 case 5:
+                    int optLVL5 = 0;
+                    do {
+                        optLVL5 = Integer.parseInt(JOptionPane.showInputDialog("Seleccione el ejercicio que desea ejecutar: "
+                                + "\n1. Avanzado int"
+                                + "\n2. Avanzado double"
+                                + "\n3. Avanzado int"
+                                + "\n4. Avanzado int"
+                                + "\n5. Regresar al menu principal"));
+                        switch (optLVL5) {
+                            case 1:
+                                break;
+                            case 2:
+                                break;
+                            case 3:
+                                break;
+                            case 4:
+                                break;
+                            case 5:
+                                JOptionPane.showMessageDialog(null, "REGRESANDO AL MENÚ PRINCIPAL");
+                                break;
+                            default:
+                                JOptionPane.showMessageDialog(null, "No disponible.");
+                        }
+                    } while (optLVL5 != 5);
                     break;
                 case 0:
                     JOptionPane.showMessageDialog(null, "Gracias por usar el sistema");
